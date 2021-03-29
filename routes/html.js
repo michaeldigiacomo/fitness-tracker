856ed7ff38d36b5f71f8b79b2499d615fb357048
Michael DiGiacomo <mdigiacomo05@me.com>
2021-03-29 16:17:32 -0400

    added .env and gitignore files, minor addition in server.js and added routing to stats page

--- routes/html.js
+++ routes/html.js
@@ -5,7 +5,11 @@ router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname,"../public/exercise.html"));
 })
 
-router.get("/exercise", (req, res) => {
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"../public/home.html"));
+})
+
+router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname,"../public/stats.html"));
 })
 
